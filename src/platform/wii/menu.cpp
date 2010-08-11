78b9dbeadfb97ff95e59e64baa09be6f20385cdc
dborth <dborth@01ee5fa4-3754-11de-8a2e-13aa1706fec1>
2010-08-11 05:40:12 +0000

    bump version

--- src/platform/wii/menu.cpp
+++ src/platform/wii/menu.cpp
@@ -20,7 +20,7 @@
 #include "wiihardware.h"
 
 #define THREAD_SLEEP 100
-#define APPVERSION 		"1.3"
+#define APPVERSION 		"1.4"
 
 static GuiImageData * pointer[4];
 static GuiWindow * mainWindow = NULL;
