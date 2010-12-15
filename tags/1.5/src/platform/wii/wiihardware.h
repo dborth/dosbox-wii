4af736283437a986f8a7a6257eefdec9b18f8857
dborth <dborth@01ee5fa4-3754-11de-8a2e-13aa1706fec1>
2010-12-15 02:45:59 +0000

    [1.5 - December 14, 2010]
    * Updated to latest DOSBox SVN
    * USB compatibility improvements
    * Fix tilde key on keyboard

---
+++ tags/1.5/src/platform/wii/wiihardware.h
@@ -0,0 +1,18 @@
+/****************************************************************************
+ * DOSBox Wii
+ * Tantric 2009-2010
+ ***************************************************************************/
+
+#ifndef WIIHARDWARE_H
+#define WIIHARDWARE_H
+#include <wiiuse/wpad.h>
+
+void WiiInit();
+void WiiMenu();
+void CreateAppPath(char origpath[]);
+void WiiFinished();
+
+extern char appPath[1024];
+extern char dosboxCommand[1024];
+
+#endif
