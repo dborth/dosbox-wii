e031fac55632ef43423da57da6213ff8159087b5
dborth <dborth@01ee5fa4-3754-11de-8a2e-13aa1706fec1>
2011-07-06 16:32:42 +0000

    [1.6 - July 6, 2011]
    * Updated to latest DOSBox SVN
    * Updated to latest libogc and devkitPPC

---
+++ tags/1.6/src/platform/wii/filelist.h
@@ -0,0 +1,106 @@
+/****************************************************************************
+ * DOSBox Wii Home Menu
+ * Tantric 2009-2010
+ *
+ * filelist.h
+ *
+ * Contains a list of all of the files stored in the images/, fonts/, and
+ * sounds/ folders
+ ***************************************************************************/
+
+#ifndef _FILELIST_H_
+#define _FILELIST_H_
+
+#include <gccore.h>
+
+extern const u8		font_ttf[];
+extern const u32	font_ttf_size;
+
+extern const u8		enter_ogg[];
+extern const u32	enter_ogg_size;
+
+extern const u8		exit_ogg[];
+extern const u32	exit_ogg_size;
+
+extern const u8		button_over_pcm[];
+extern const u32	button_over_pcm_size;
+
+extern const u8		button_click_pcm[];
+extern const u32	button_click_pcm_size;
+
+extern const u8		logo_png[];
+extern const u32	logo_png_size;
+
+extern const u8		logo_over_png[];
+extern const u32	logo_over_png_size;
+
+extern const u8		bg_top_png[];
+extern const u32	bg_top_png_size;
+
+extern const u8		bg_bottom_png[];
+extern const u32	bg_bottom_png_size;
+
+extern const u8		button_png[];
+extern const u32	button_png_size;
+
+extern const u8		button_over_png[];
+extern const u32	button_over_png_size;
+
+extern const u8		button_small_png[];
+extern const u32	button_small_png_size;
+
+extern const u8		button_small_over_png[];
+extern const u32	button_small_over_png_size;
+
+extern const u8		button_large_png[];
+extern const u32	button_large_png_size;
+
+extern const u8		button_large_over_png[];
+extern const u32	button_large_over_png_size;
+
+extern const u8		battery_png[];
+extern const u32	battery_png_size;
+
+extern const u8		battery_red_png[];
+extern const u32	battery_red_png_size;
+
+extern const u8		battery_bar_png[];
+extern const u32	battery_bar_png_size;
+
+extern const u8		credits_box_png[];
+extern const u32	credits_box_png_size;
+
+extern const u8		keyboard_textbox_png[];
+extern const u32	keyboard_textbox_png_size;
+
+extern const u8		keyboard_key_png[];
+extern const u32	keyboard_key_png_size;
+
+extern const u8		keyboard_key_over_png[];
+extern const u32	keyboard_key_over_png_size;
+
+extern const u8		keyboard_mediumkey_png[];
+extern const u32	keyboard_mediumkey_png_size;
+
+extern const u8		keyboard_mediumkey_over_png[];
+extern const u32	keyboard_mediumkey_over_png_size;
+
+extern const u8		keyboard_largekey_png[];
+extern const u32	keyboard_largekey_png_size;
+
+extern const u8		keyboard_largekey_over_png[];
+extern const u32	keyboard_largekey_over_png_size;
+
+extern const u8		player1_point_png[];
+extern const u32	player1_point_png_size;
+
+extern const u8		player2_point_png[];
+extern const u32	player2_point_png_size;
+
+extern const u8		player3_point_png[];
+extern const u32	player3_point_png_size;
+
+extern const u8		player4_point_png[];
+extern const u32	player4_point_png_size;
+
+#endif
